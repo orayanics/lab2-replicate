daa65502d888fee5209417e6363757529de6d7c2
orayanics <nicole.oraya.cics@ust.edu.ph>
2024-02-19 14:43:46 +0800

    sfa

--- src/components/Song.js
+++ src/components/Song.js
@@ -2,6 +2,9 @@ import React from "react";
 import { tracks } from "../assets/tracks.js";
 import "../styles/song.css";
 export default function Song() {
+  const mostPlayedTrack = tracks.reduce((prev, current) =>
+    prev.plays > current.plays ? prev : current
+  );
   return (
     <>
       <div>
@@ -16,6 +19,9 @@ export default function Song() {
               </div>
 
               <div className="flex-container two">
+                {track === mostPlayedTrack ? (
+                  <p className="most-played">Most Played</p>
+                ) : null}{" "}
                 <p className="span">
                   {track.plays.toLocaleString("en-US").replace(/,/g, ",")}
                 </p>
